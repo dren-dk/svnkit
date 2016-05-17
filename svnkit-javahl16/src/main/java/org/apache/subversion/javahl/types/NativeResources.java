084132464d275856d11682490d4d6564cc9e0ee5
Dmitry Pavlenko <dmitry.pavlenko@tmatesoft.com>
2016-05-17 09:36:29 +0000

    SVNKIT-662: "UnsatisfiedLinkError: no svnjavahl-1 in java.library.path" error fixed by package injection.
    Native SVN java classes call Version class static initializer that in its turn calls
    NativeResources#loadNativeLibrary method that fails (because SVNKit
    implements everything in java and has no native library).

---
+++ svnkit-javahl16/src/main/java/org/apache/subversion/javahl/types/NativeResources.java
@@ -0,0 +1,12 @@
+package org.apache.subversion.javahl.types;
+
+public class NativeResources {
+
+    public static Version getVersion() {
+        return null;
+    }
+
+    public static void loadNativeLibrary() {
+        //see SVNKIT-662 for details
+    }
+}
