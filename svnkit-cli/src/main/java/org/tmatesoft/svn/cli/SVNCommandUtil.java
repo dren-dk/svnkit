bb6e4a7445044f1f9b18b19d059be794ce26d5a7
Alexander Kitaev <alexander.kitaev@tmatesoft.com>
2016-10-10 11:16:45 +0000

    Copyright year updated in version output.

--- svnkit-cli/src/main/java/org/tmatesoft/svn/cli/SVNCommandUtil.java
+++ svnkit-cli/src/main/java/org/tmatesoft/svn/cli/SVNCommandUtil.java
@@ -463,7 +463,7 @@ public class SVNCommandUtil {
         }
         if (!quiet) {
             message += 
-                "\nCopyright (c) 2004-2012 TMate Software.\n" +
+                "\nCopyright (c) 2004-2016 TMate Software.\n" +
                 "SVNKit is an Open Source software, see http://svnkit.com/ for more information.\n" +
                 "SVNKit is a pure Java (TM) version of Subversion, see http://subversion.tigris.org/";
         }
