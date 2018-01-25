067d0c26b3c63344045828683be71baec9622375
Dmitry Pavlenko <pavlenko@tmatesoft.com>
2018-01-25 17:38:44 +0100

    'SVN' class was by mistake replaced with a lock testing content, now fixed.

--- svnkit-cli/src/main/java/org/tmatesoft/svn/cli/SVN.java
+++ svnkit-cli/src/main/java/org/tmatesoft/svn/cli/SVN.java
@@ -12,13 +12,6 @@
 package org.tmatesoft.svn.cli;
 
 
-import java.io.File;
-import java.io.IOException;
-
-import org.tmatesoft.svn.core.SVNException;
-import org.tmatesoft.svn.core.internal.util.SVNDoubleLock;
-import org.tmatesoft.svn.core.internal.util.jna.SVNJNAUtil;
-
 /**
  * @version 1.3
  * @author  TMate Software Ltd.
@@ -26,33 +19,7 @@ import org.tmatesoft.svn.core.internal.util.jna.SVNJNAUtil;
 public class SVN {
 
     public static void main(String[] args) {
-        try {
-            run();
-        } catch (SVNException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        org.tmatesoft.svn.cli.svn.SVN.main(args);
     }
 
-    private static void run() throws SVNException, IOException {
-        SVNJNAUtil.setJNAEnabled(false);
-
-        final File file = new File("/tmp/file");
-
-        System.out.println("Obtaining lock");
-        final SVNDoubleLock lock = SVNDoubleLock.obtain(file, true);
-        System.out.println("lock = " + lock);
-        lock.release();
-        System.out.println("lock = " + lock);
-
-
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
