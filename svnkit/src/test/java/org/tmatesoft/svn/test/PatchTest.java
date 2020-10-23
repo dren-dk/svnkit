4f98d989177ea7c14fac99424e28af0f97c164e6
Dmitry Pavlenko <pavlenko@tmatesoft.com>
2020-10-23 16:55:10 +0200

    SVNKIT-756: Added a couple of reproducing tests, currently fail.

--- svnkit/src/test/java/org/tmatesoft/svn/test/PatchTest.java
+++ svnkit/src/test/java/org/tmatesoft/svn/test/PatchTest.java
@@ -42,6 +42,11 @@ import org.tmatesoft.svn.core.wc2.SvnTarget;
 
 public class PatchTest {
 
+    private static final char infinity = '\u221e';
+    private static final String infinityString = String.valueOf(infinity);
+    private static final char euro = '\u20ac';
+    private static final String euroString = String.valueOf(euro);
+
     @Test
     public void testModification() throws Exception {
         final TestOptions options = TestOptions.getInstance();
@@ -1147,6 +1152,124 @@ public class PatchTest {
             Assert.assertEquals(SVNStatusType.STATUS_MODIFIED, fileStatus.getNodeStatus());
             Assert.assertEquals(SVNStatusType.STATUS_NONE, fileStatus.getPropertiesStatus());
 
+        } finally {
+            svnOperationFactory.dispose();
+            sandbox.dispose();
+        }
+    }
+
+    @Test
+    public void testNonAsciiCharacterUnchanged() throws Exception {
+        //SVNKIT-756
+        final TestOptions options = TestOptions.getInstance();
+
+        final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+        final Sandbox sandbox = Sandbox.createWithCleanup(getTestName() + ".testNonAsciiCharacters", options);
+        try {
+            final SVNURL url = sandbox.createSvnRepository();
+
+            final CommitBuilder commitBuilder1 = new CommitBuilder(url);
+            commitBuilder1.addFile("file", (euroString + "\n" + "old").getBytes());
+            commitBuilder1.commit();
+
+            final CommitBuilder commitBuilder2 = new CommitBuilder(url);
+            commitBuilder2.changeFile("file", (euroString + "\n" + "new").getBytes());
+            commitBuilder2.commit();
+
+            final SvnDiffGenerator diffGenerator = new SvnDiffGenerator();
+            diffGenerator.setBasePath(new File("").getAbsoluteFile());
+            final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            final SvnDiff diff = svnOperationFactory.createDiff();
+            diff.setSource(SvnTarget.fromURL(url), SVNRevision.create(1), SVNRevision.create(2));
+            diff.setDiffGenerator(diffGenerator);
+            diff.setOutput(output);
+            diff.run();
+
+            final String patchString = output.toString();
+
+            final WorkingCopy workingCopy = sandbox.checkoutNewWorkingCopy(url, 1);
+            final File workingCopyDirectory = workingCopy.getWorkingCopyDirectory();
+            final File file = workingCopy.getFile("file");
+            final File patchFile = new File(sandbox.createDirectory("directory"), "patchFile");
+
+            TestUtil.writeFileContentsString(patchFile, patchString);
+
+            final UpdateTest.EventsHandler eventHandler = new UpdateTest.EventsHandler();
+            svnOperationFactory.setEventHandler(eventHandler);
+
+            final SvnPatch patch = svnOperationFactory.createPatch();
+            patch.setSingleTarget(SvnTarget.fromFile(workingCopyDirectory));
+            patch.setPatchFile(patchFile);
+            patch.run();
+
+            Assert.assertEquals((euroString + "\n" + "new"), TestUtil.readFileContentsString(file));
+
+            final List<SVNEvent> events = eventHandler.getEvents();
+            Assert.assertEquals(2, events.size());
+            Assert.assertEquals(SVNEventAction.PATCH, events.get(0).getAction());
+            Assert.assertEquals(file, events.get(0).getFile());
+            Assert.assertEquals(SVNEventAction.PATCH_APPLIED_HUNK, events.get(1).getAction());
+            Assert.assertEquals(file, events.get(1).getFile());
+        } finally {
+            svnOperationFactory.dispose();
+            sandbox.dispose();
+        }
+    }
+
+    @Test
+    public void testNonAsciiCharacterChanged() throws Exception {
+        //SVNKIT-756
+        final TestOptions options = TestOptions.getInstance();
+
+        final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+        final Sandbox sandbox = Sandbox.createWithCleanup(getTestName() + ".testNonAsciiCharacters", options);
+        try {
+            final SVNURL url = sandbox.createSvnRepository();
+
+            final CommitBuilder commitBuilder1 = new CommitBuilder(url);
+            commitBuilder1.addFile("file", (euroString + "\n" + infinityString + "old").getBytes());
+            commitBuilder1.commit();
+
+            final CommitBuilder commitBuilder2 = new CommitBuilder(url);
+            commitBuilder2.changeFile("file", (euroString + "\n" + infinityString + infinityString + "new").getBytes());
+            commitBuilder2.commit();
+
+            final SvnDiffGenerator diffGenerator = new SvnDiffGenerator();
+            diffGenerator.setBasePath(new File("").getAbsoluteFile());
+            final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            final SvnDiff diff = svnOperationFactory.createDiff();
+            diff.setSource(SvnTarget.fromURL(url), SVNRevision.create(1), SVNRevision.create(2));
+            diff.setDiffGenerator(diffGenerator);
+            diff.setOutput(output);
+            diff.run();
+
+            final String patchString = output.toString();
+
+            final WorkingCopy workingCopy = sandbox.checkoutNewWorkingCopy(url, 1);
+            final File workingCopyDirectory = workingCopy.getWorkingCopyDirectory();
+            final File file = workingCopy.getFile("file");
+            final File patchFile = new File(sandbox.createDirectory("directory"), "patchFile");
+
+            TestUtil.writeFileContentsString(patchFile, patchString);
+
+            final UpdateTest.EventsHandler eventHandler = new UpdateTest.EventsHandler();
+            svnOperationFactory.setEventHandler(eventHandler);
+
+            final SvnPatch patch = svnOperationFactory.createPatch();
+            patch.setSingleTarget(SvnTarget.fromFile(workingCopyDirectory));
+            patch.setPatchFile(patchFile);
+            patch.run();
+
+            Assert.assertEquals((euroString + "\n" + infinityString + infinityString + "new"), TestUtil.readFileContentsString(file));
+
+            final List<SVNEvent> events = eventHandler.getEvents();
+            Assert.assertEquals(2, events.size());
+            Assert.assertEquals(SVNEventAction.PATCH, events.get(0).getAction());
+            Assert.assertEquals(file, events.get(0).getFile());
+            Assert.assertEquals(SVNEventAction.PATCH_APPLIED_HUNK, events.get(1).getAction());
+            Assert.assertEquals(file, events.get(1).getFile());
         } finally {
             svnOperationFactory.dispose();
             sandbox.dispose();
